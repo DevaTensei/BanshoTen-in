f5136acb1594708b71171e785e5e3a2a9f799627
DevaTensei <58239311+DevaTensei@users.noreply.github.com>
2019-11-27 01:19:28 -0300

    Add files via upload

---
+++ Bank Account .py
@@ -0,0 +1,104 @@
+print("█████████ ")
+print(" █▄█████▄█ ")
+print( "█▼▼▼▼▼")
+print( " █ ")
+print("  █Teste Bank Account py ")
+print("  █    kaio tomaz")
+print("  █▲▲▲▲▲ ")
+print( " █████████ ")
+print("    ██ ██ ")
+
+
+
+print("Acessando repo:github:kaiotomaz/devatensei")
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+
+
+
+print("Sistema Inicializando")
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+
+print("Sistema Inicializando…")
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+
+print("Sistema Inicializando……")
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+
+print("Sistema Inicializando………")
+from  time import sleep 
+
+for contagem in range(0,2):
+    sleep(1)
+    
+    print("Entrando em contato com o servidor")
+from  time import sleep 
+
+for contagem in range(0,2):
+    sleep(1)
+    
+print("Entrando em contato com o servidor…………")
+from  time import sleep 
+
+for contagem in range(0,2):
+    sleep(1)
+
+user =	input("Digite seu nome :")
+nc = input("Muito bem {}, Digite o numero do seu cartão :".format(user))
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+dv =	input("Digite a data vêncimento :")
+from  time import sleep 
+
+for contagem in range(0,3):
+    sleep(1)
+    
+conf = input("Confirme ⁿ{} đ{} Correto ? ".format(nc ,dv))
+from  time import sleep 
+
+for contagem in range(0,2):
+    sleep(1)
+depo = input("Sr(a) {} , Informe o valor de deposito:".format(user))
+
+from  time import sleep 
+
+for contagem in range(0,5):
+    sleep(1)
+    
+print("Transferindo {}R$ ".format(depo))
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+print("Transferindo {}R$ *** ".format(depo))
+from  time import sleep 
+
+for contagem in range(0,2):
+    sleep(1)
+    
+    print("Transferindo {}R$ ******** ".format(depo))
+from  time import sleep 
+
+for contagem in range(0,3):
+    sleep(1)
+
+print("Foram transferidos {}R$ para: {}".format(depo ,user))
+from  time import sleep 
+
+for contagem in range(0,1):
+    sleep(1)
+    
+print("Encerrando atividade")
