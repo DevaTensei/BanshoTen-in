64f9b7ceff473c52d2a8820e5e9a27a7cfe505f0
DevaTensei <58239311+DevaTensei@users.noreply.github.com>
2019-11-29 08:18:50 -0300

    Add files via upload

---
+++ Sixpaths.py
@@ -0,0 +1,110 @@
+#impot
+import androidhelper
+import platform
+import os
+import sys
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+droid = androidhelper.Android()
+droid.ttsSpeak("Carregando Modulos")
+#PEGAR A VERSÃO COMPLETA
+print(platform.platform())
+#PEGAR O SISTEMA OPERACIONAL
+print(platform.system())
+#PEGAR A VERSÃO DO SISTEMA OPERACIONAL
+print(platform.release())
+#PEGAR A VERSÃO COMPLETA DO SISTEMA OPERACIONAL
+print(platform.version())
+#Logo Six Path
+print("     ___  ")
+print("    / _ \ ")
+print("   | (_) |")
+print("    \__, |")
+print("      /_/ ")
+from  time import sleep 
+for contagem in range(0,1):
+    sleep(1)
+print("  ___     ___  ")
+print(" / _ \   / _ \ ")
+print(" | (_) | | (_) | ")
+print(" \__, | \__, |")
+print("    /_/    /_/ ")
+from  time import sleep 
+for contagem in range(0,1):
+    sleep(1)
+print("  ___   ___   ___    ___ ")
+print(" / _ \  / _ \  / _ \ / _ \ ")
+print(" | (_) || (_) || (_) || (_) |")
+print(" \__, | \__, | \__, | \__, |")
+print("   /_/    /_/    /_/    /_/")
+from  time import sleep 
+for contagem in range(0,1):
+    sleep(1)
+print("  ___   ___   ___    ___   ___     ___")
+print(" / _ \  / _ \  / _ \ / _ \ / _ \ /  _  \ ")
+print(" | (_) || (_) || (_) || (_) ||(_)|  |(_) | ")
+print(" \__, | \__, | \__, | \__, |\__, | \__, |")
+print("   /_/    /_/    /_/    /_/   /_/    /_/")
+droid = androidhelper.Android()
+droid.ttsSpeak("Digite a credencial de acesso")
+#root
+root = input("Digite a credencial de acesso : ")
+if root=="root":
+	print("\033[1;32;40m Acesso Aprovado  \n")
+	droid.ttsSpeak("Acesso aprovado")
+	os.system('clear') or None
+else:
+	print("\033[1;31;40m Acesso Negado  \n")
+	droid.ttsSpeak("Acesso Negado")
+	restart_program()
+	os.system('clear') or None
+from  time import sleep 
+for contagem in range(0,1):
+    sleep(1)
+    #Clear
+#Anti Loading
+print("Inicializando Bansho Tennin Os../ ")
+#Clear
+os.system('clear') or None
+print("Inicializando Bansho Tennin Os..| ")
+#Clear
+os.system('clear') or None
+print("Inicializando Bansho Tennin Os..\ ")
+#Clear
+os.system('clear') or None
+print("Inicializando Bansho Tennin Os..| ")
+#Clear
+os.system('clear') or None
+print("Inicializando Bansho Tennin Os../ ")
+#Clear
+os.system('clear') or None
+print("Inicializando Bansho Tennin Os..| ")
+#Clear
+os.system('clear') or None
+#Logo Bansho Tenin
+
+print(" █▀▄ ▄▀▄ █▄░█ ▄▀▀ █░░ ▄▀▄")
+print(" █▀█ █▀█ █░▀█ ░▀▄ █▀▄ █░█ ")
+print(" ▀▀░ ▀░▀ ▀░░▀ ▀▀░ ▀░▀ ░▀░ ")
+print(" ▀█▀ █▀▀ █▄─█ ▀ █▄─█")
+print(" ─█─ █▀▀ █─▀█ █ █─▀█")
+print(" ─▀─ ▀▀▀ ▀──▀ ▀ ▀──▀")
+print(" ▄▀▄ ▄▀▀")
+print(" █─█ ─▀▄")
+print(" ─▀─ ▀▀─")
+#Boot
+print(platform.version())
+print("Bem Vindo")
+#Faz o Celular Falar
+droid = androidhelper.Android()
+droid.ttsSpeak("Bem vindo ao Bancho Tenin")
+#Console
+while(True):
+  cmd=input("-->")
+  if (cmd==""): break;
+  modcmd(cmd)
+
+
+
+
